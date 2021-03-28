f0970022c3be59cebeb2d58e8d1d99c2b4795bd7
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-03-28 22:17:09 +0300

    Выполнены задачи по уроку 3.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         com.company.lesson1.Main.main();
         com.company.lesson2.Main.main();
+        com.company.lesson3.Main.main();
     }
 
 }
