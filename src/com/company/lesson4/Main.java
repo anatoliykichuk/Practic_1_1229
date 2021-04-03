64e9fbfb7fa14612f779efcfa15895433d9c652b
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-04-03 20:13:57 +0300

    Работающее решение

--- src/com/company/lesson4/Main.java
+++ src/com/company/lesson4/Main.java
@@ -1,7 +1,9 @@
 package com.company.lesson4;
 
 import jdk.nashorn.internal.ir.ReturnNode;
+import sun.audio.AudioPlayer;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -23,49 +25,68 @@ public class Main {
         initializePlayingMAp();
         displayPlayingMap();
 
-        boolean isWin = isWin(EMPTY_TAG);
+        boolean isNotWin = !isWin(EMPTY_TAG);
 
-        while (isWin) {
-            if (makeUserMove()) break;
-            if (makePCMove()) break;
+        while (isNotWin) {
+            makeUserMove();
+
+            if (isWin(USER_TAG)) {
+                System.out.println("Поздравляем! Вы победили!");
+                break;
+            }
+
+            if (isNobodyWon()) {
+                System.out.println("Ничья!");
+                break;
+            }
+
+            makePCMove();
+
+            if (isWin(PC_TAG)) {
+                System.out.println("Победил ПК...");
+                break;
+            }
+
+            if (isNobodyWon()) {
+                System.out.println("Ничья!");
+                break;
+            }
         }
         scanner.close();
     }
 
-    public static boolean makeUserMove() {
+    public static void makeUserMove() {
         int row, column;
         boolean isAdressIncorect;
 
-        System.out.println("Ваш ход");
+        System.out.println("Ваш ход...");
 
         do {
             System.out.println("Укажите адрес ячейки:");
             System.out.print("- номер строки: ");
-            row = scanner.nextInt();
+            row = scanner.nextInt() - 1;
 
             System.out.print("- номер колонки: ");
-            column = scanner.nextInt();
+            column = scanner.nextInt() - 1;
 
             isAdressIncorect = isAdressIncorect(row, column);
 
             if (isAdressIncorect) {
-                System.out.println("Адрес некорректен - повторите попытку.");
+                System.out.println("\nАдрес некорректен - повторите попытку...");
             } else {
-                playing_map[row - 1][column - 1] = USER_TAG;
+                playing_map[row][column] = USER_TAG;
             }
 
         } while (isAdressIncorect);
 
         displayPlayingMap();
-
-        return isWin(USER_TAG);
     }
 
-    public static boolean makePCMove() {
+    public static void makePCMove() {
         int row, column;
         boolean isAdressIncorect;
 
-        System.out.println("Ход ПК");
+        System.out.println("Ход ПК...");
 
         Random random = new Random();
 
@@ -76,20 +97,18 @@ public class Main {
             isAdressIncorect = isAdressIncorect(row, column);
 
             if (!isAdressIncorect) {
-                playing_map[row - 1][column - 1] = PC_TAG;
+                playing_map[row][column] = PC_TAG;
             }
 
         } while (isAdressIncorect);
 
         displayPlayingMap();
-
-        return isWin(PC_TAG);
     }
 
     public static boolean isAdressIncorect(int row, int column) {
-        return row < 1 || row > playing_map.length ||
-                column < 1 || column > playing_map.length ||
-                playing_map[row - 1][column - 1] != EMPTY_TAG;
+        return row < 0 || row >= playing_map.length ||
+                column < 0 || column >= playing_map.length ||
+                playing_map[row][column] != EMPTY_TAG;
     }
 
     public static boolean isWin(char tag) {
@@ -97,37 +116,60 @@ public class Main {
             return false;
         }
 
-        int tagsCountOnRow;
-        int tagsCountOnColumn;
         int tagsCountOnDirectlyDiagonal = 0;
         int tagsCountOnReverselyDiagonal = 0;
 
-        for (int externalIndex = 0; externalIndex <= playing_map.length; externalIndex++) {
-            tagsCountOnRow = 0;
-            tagsCountOnColumn = 0;
+        for (int externalIndex = 0; externalIndex < playing_map.length; externalIndex++) {
+            int tagsCountOnRow = 0;
+            int tagsCountOnColumn = 0;
 
             if (playing_map[externalIndex][externalIndex] == tag) {
                 tagsCountOnDirectlyDiagonal++;
+
+                if (tagsCountOnDirectlyDiagonal == POINTS_TO_WIN &&
+                        (playing_map[0][0] != tag ||
+                                playing_map[playing_map.length - 1][playing_map.length - 1] != tag)) return true;
             }
 
-            if (playing_map[externalIndex][playing_map.length - externalIndex] == tag) {
+            if (playing_map[externalIndex][playing_map.length - externalIndex - 1] == tag) {
                 tagsCountOnReverselyDiagonal++;
+
+                if (tagsCountOnReverselyDiagonal == POINTS_TO_WIN &&
+                        (playing_map[0][playing_map.length - 1] != tag ||
+                                playing_map[playing_map.length - 1][0] != tag)) return true;
             }
 
-            for (int internalIndex = 0; internalIndex <= playing_map.length; internalIndex++) {
+            for (int internalIndex = 0; internalIndex < playing_map.length; internalIndex++) {
+
                 if (playing_map[externalIndex][internalIndex] == tag) {
                     tagsCountOnRow++;
+
+                    if (tagsCountOnRow == POINTS_TO_WIN &&
+                            (playing_map[externalIndex][0] != tag ||
+                                    playing_map[externalIndex][playing_map.length - 1] != tag)) return true;
                 }
 
                 if (playing_map[internalIndex][externalIndex] == tag) {
                     tagsCountOnColumn++;
+
+                    if (tagsCountOnColumn == POINTS_TO_WIN &&
+                            (playing_map[0][externalIndex] != tag ||
+                                    playing_map[playing_map.length - 1][externalIndex] != tag)) return true;
                 }
             }
-            if (tagsCountOnRow == POINTS_TO_WIN || tagsCountOnColumn == POINTS_TO_WIN) return true;
         }
         if (tagsCountOnDirectlyDiagonal == POINTS_TO_WIN || tagsCountOnReverselyDiagonal == POINTS_TO_WIN) return true;
 
         return false;
+    }
+
+    public static boolean isNobodyWon() {
+        for (int index = 0; index < playing_map.length; index++) {
+            if (playing_map[index][index] == EMPTY_TAG) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void initializePlayingMAp() {
