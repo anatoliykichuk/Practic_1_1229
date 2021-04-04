64eb9ba4c695e8dd75b774a8e113737ddcd86b9a
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-04-04 12:00:19 +0300

    Создан класс Сортрудник с описанием полей и объявлением конструктора.

---
+++ src/com/company/lesson5/Empoyee.java
@@ -0,0 +1,27 @@
+package com.company.lesson5;
+
+public class Empoyee {
+
+    /*
+    1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+    2. Конструктор класса должен заполнять эти поля при создании объекта.
+    3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
+    4. Создать массив из 5 сотрудников.
+    5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
+    */
+
+    private String surename;
+    private String name;
+    private String patronymic;
+    private int age;
+    private String phoneNumber;
+    private String email;
+    private String function;
+    private double salary;
+
+    Empoyee(String surename, String name, String patronymic, int age,
+            String phoneNumber, String email, String function, double salary) {
+
+    }
+
+}
---
+++ src/com/company/lesson5/Main.java
@@ -0,0 +1,9 @@
+package com.company.lesson5;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
