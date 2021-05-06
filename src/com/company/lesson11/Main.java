c95e77563abceaed76dd8c5fd00202db6ab71648
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-05-06 15:51:28 +0300

    11 урок: Реализованы п.п. 1, 2

---
+++ src/com/company/lesson11/Main.java
@@ -0,0 +1,53 @@
+package com.company.lesson11;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main() {
+        Object[] data = getData();
+        showData(data);
+
+        replaceData(data, 2, 4);
+        showData(data);
+
+        ArrayList<Object> list = castArrayToArrayList(data);
+        System.out.println(list.toString());
+    }
+
+    public static void replaceData(Object[] data, int index1, int index2) throws ArrayIndexOutOfBoundsException {
+        if (index1 >= data.length && index2 >= data.length) throw new ArrayIndexOutOfBoundsException();
+
+        Object tmp = data[index1];
+        data[index1] = data[index2];
+        data[index2] = tmp;
+    }
+
+    public static void showData(Object[] data) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+
+        for (int index = 0; index < data.length; index++) {
+            String separator = (index == data.length - 1) ? "" : ", ";
+            builder.append(data[index].toString()).append(separator);
+        }
+        builder.append("]");
+        System.out.println(builder.toString());
+    }
+
+    public static Object[] getData() {
+        return new Object[] {
+                new Object(),
+                new Integer(10),
+                new Integer(5),
+                new Object(),
+                new Double(15D)
+        };
+    }
+
+    public static ArrayList<Object> castArrayToArrayList(Object[] array) {
+        return new ArrayList<>(Arrays.asList(array));
+    }
+
+}
