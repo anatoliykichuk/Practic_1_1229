e1531f6171e8184fdd851afaa42069bba62e1a8e
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-05-10 15:29:50 +0300

    14 урок: Написан тест

---
+++ src/test/java/com/company/lesson14/MainTest.java
@@ -0,0 +1,26 @@
+package com.company.lesson14;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void extractNumbersCriterionValueIsPresent() {
+        int[] numbers = new int[] {15, 4, 19, 4, 25};
+        int[] expectedResult = new int[] {25};
+        int[] exctractedNUmbers = Main.extractNumbers(numbers);
+
+        Assertions.assertArrayEquals(expectedResult, exctractedNUmbers);
+    }
+
+    @Test
+    void extractNumbersLastCriterionValue() {
+        int[] numbers = new int[] {15, 3, 19, 9, 4};
+        int[] expectedResult = new int[0];
+        int[] exctractedNUmbers = Main.extractNumbers(numbers);
+
+        Assertions.assertArrayEquals(expectedResult, exctractedNUmbers);
+    }
+
+}
